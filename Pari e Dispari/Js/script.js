dbdb17d3a0f0421bb809031d77ed76027b61f4bc
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-15 00:14:39 +0100

    feat: add pari o dispari

---
+++ Pari e Dispari/Js/script.js
@@ -0,0 +1,36 @@
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+//Ricavo i dati dall'utente
+const userEvenOrOdd = prompt('Pari o Dispari?');
+console.log('utente - ' + userEvenOrOdd);
+const userNumber = parseInt(prompt('Segli un numero da 1 a 5'));
+console.log('utente - ' + userNumber);
+//Genero numero per pc
+const pcNumber = Math.floor(Math.random() * 5) + 1;
+console.log('pc - ' + pcNumber);
+//Sommo i numeri
+const sumNumber = userNumber + pcNumber;
+console.log('somma - ' + sumNumber);
+//Creo una funzione che stabilisca se la somma è pari o dispari
+function isEvenOrOdd (num1, num2, sum) {
+    if (sum % 2 === 0) {
+        sum = 'Pari';
+    } else {
+        sum = 'Dispari';
+    }
+    return sum;
+}
+isEvenOrOdd (userNumber, pcNumber, sumNumber);
+console.log(isEvenOrOdd);
+let userMessage;
+isEvenOrOdd = userMessage;
+//Dichiaro chi ha vinto
+if (userMessage === userEvenOrOdd) {
+    alert('Hai vinto!');
+} else {
+    alert('Hai perso!');
+}
