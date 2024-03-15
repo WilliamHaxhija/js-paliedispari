774db6b443410dc003db3dff4281849a740b3cf2
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-15 01:03:02 +0100

    feat: add palindromo

--- Palindroma/Js/script.js
+++ Palindroma/Js/script.js
@@ -1,2 +1,22 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+const userWord = prompt('Inserisci una parola.')
+console.log(userWord);
+console.log(userWord.length)
+function isPalindrome (word) {
+    let palindrome = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+        let thisLetter = word[i];
+        console.log(thisLetter);
+        palindrome += thisLetter;  
+    }
+    console.log(palindrome);
+    return palindrome;
+}
+if (userWord === isPalindrome(userWord)) {
+    alert('Palindromo!')
+} else {
+    alert('Non è un palindromo!')
+}
+
